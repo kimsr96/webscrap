e06bd3bbf5ec24542cb01b283bc6330dac5a464a
Seungryeol <kimsr960@gmail.com>
2021-01-06 14:11:10 +0900

    12

--- webscraping_basic/9_bs4_coupang.py
+++ webscraping_basic/9_bs4_coupang.py
@@ -8,4 +8,40 @@ res = requests.get(url, headers=headers)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text,"lxml")
-print(res.text) 
+# print(res.text) 
+
+items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
+# print(item[1].find("div", attrs={"class":"name"}).get_text())
+for item in items:
+
+    # 광고 제품은 제외
+    ad_badge = item.find("span", attrs={"class":"ad-badge-text"})
+    if ad_badge:
+        print("  <광고 상품 제외합니다> ")
+    name = item.find("div", attrs={"class":"name"}).get_text()
+
+    #애플 제품 제외
+    if "Apple" in name:
+        print(" <Apple 상품 제외합니다>")
+        continue
+
+    price = item.find("strong", attrs={"class":"price-value"}).get_text()
+
+    # 리뷰 100개 이상, 평점 4.5이상 되는 것만 조회
+    rate = item.find("em", attrs={"class":"rating"})
+    if rate:
+        rate = rate.get_text()
+    else:
+        print("  <평점 없는 상품 제외합니다> ")
+        continue
+
+    rate_cnt = item.find("span", attrs={"class":"rating-total-count"})
+    if rate_cnt:
+        rate_cnt = rate_cnt.get_text()
+        rate_cnt = rate_cnt[1:-1]
+    else:
+        print("  <평점 없는 상품 제외합니다> ")
+        continue
+
+    if float(rate) >= 4.5 and int(rate_cnt) >= 100: 
+        print(name, price, rate, rate_cnt)
